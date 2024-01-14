c07abe1043a7f7c13b7e3b5f4e56b9a503d7e11a
Umar Faruq <umarfaruq@Steph30Air.local>
2024-01-14 11:39:00 +0700

    update tailwind css installation

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,17 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
-  parserOptions: {
-    parser: '@babel/eslint-parser',
-    requireConfigFile: false
-  },
-  extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
-  ],
-  plugins: [
-  ],
+  extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
+  plugins: [],
   // add your custom rules here
-  rules: {}
+  rules: {
+    'object-shorthand': ['warn', 'always', { avoidQuotes: true }],
+    'vue/no-v-html': 'off',
+    'vue/attribute-hyphenation': 'off',
+    camelcase: 'off',
+    'comma-dangle': 'off',
+    semi: 'off',
+  },
 }
